8ac9be9cde7eaad20d293846eaf9a15c370e9da4
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-02 11:26:56 +0500

    вывод обектов

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ Vue.createApp({
     data() {
         return {
             myHtml:  '<h1>Vue 3 APP</h1>',
-            titel: 'Hello Vue!'
+            titel: 'Hello Vue!',
+            person: {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 30
+        }
         }
     }
 }).mount('#app')
