4fcc25743ca5f272e0f20a785183655b9c246db4
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-02 12:27:20 +0500

    computed:  использовали свойства

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 Vue.createApp({
-    data() {
-        return {
-            myHtml: '<h1>Vue 3 APP</h1>',
-            title: 'Hello Vue!',
-            person: {
-                firstName: 'John',
-                lastName: 'Doe',
-                age: 30
-            },
-            items: [1, 2, 3, 4, 5, 6, 7],
-        };
-    },
-    // methods: {
-    //     stopPropagation(event) {
-    //         event.stopPropagation(); // Исправлена опечатка
-    //     }
-    // }
-}).mount('#app');
+  data() {
+    return {
+      myHtml: "<h1>Vue 3 APP</h1>",
+      title: "Hello Vue!",
+      person: {
+        firstName: "John",
+        lastName: "Doe",
+        age: 30,
+      },
+      items: [1, 2, 3, 4, 5, 6, 7],
+    };
+  },
+  computed: {
+    evenItems() {
+      return this.items.filter(i => i % 2 === 0);
+    }
+  },
+  // methods: {
+  //     stopPropagation(event) {
+  //         event.stopPropagation(); // Исправлена опечатка
+  //     }
+  // }
+}).mount("#app");
